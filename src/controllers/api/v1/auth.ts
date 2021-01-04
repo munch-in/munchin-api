f3574eab40be5113140219751c0c1037f6fefc61
Brian Lai <brianlai2014@u.northwestern.edu>
2021-01-04 15:05:17 -0600

    Add post install build

--- src/controllers/api/v1/auth.ts
+++ src/controllers/api/v1/auth.ts
@@ -2,6 +2,10 @@ import express = require('express');
 import {Container} from 'typedi';
 import AuthService from '../../../services/auth';
 
+const testRoute = function(req, res) {
+  res.send('test');
+};
+
 const login = async function (req, res) {
   const authService = Container.get(AuthService);
   const user = await authService.Login(req.body.email, req.body.password);
@@ -19,6 +23,7 @@ const register = async function (req, res) {
 };
 
 export function addRoutes(router: express.Router) {
+  router.get('/test', testRoute);
   router.post('/login', login);
   router.post('/register', register);
 }
